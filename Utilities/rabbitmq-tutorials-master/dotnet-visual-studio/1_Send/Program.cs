cad12359c3a3156d7a9d2b31786f9221069b137e
Heitor Araujo <hfa2@cin.ufpe.br>
2018-01-30 17:12:54 -0300

    Add mqtt project

---
+++ Utilities/rabbitmq-tutorials-master/dotnet-visual-studio/1_Send/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+class Program
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        factory.Port = 5672;
+        factory.UserName = "userTest";
+        factory.Password = "userTest";
+        factory.Protocol = Protocols.DefaultProtocol;
+
+        using (var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+            string message = string.Empty;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 40; i++)
+            {
+                var Timestamp = new DateTimeOffset(DateTime.UtcNow).ToString();
+                
+                
+                message = "{ \"type\": \"HPS\", \"sensorid\" : 1, \"user\": \"b8-27-eb-97-3d-b6\", \"value\":[" + 
+                        Convert.ToString(i, CultureInfo.InvariantCulture) + "," + 
+                        Convert.ToString(i + rnd.Next(-10, 11), CultureInfo.InvariantCulture) + "],\"ts\": \"" + Timestamp + "\" }";
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+                message = "{ \"type\": \"HPS\", \"sensorid\" : 2, \"user\": \"b8-27-eb-97-3d-b6\", \"value\":[" + 
+                    Convert.ToString(i , CultureInfo.InvariantCulture) + "," + 
+                    Convert.ToString(i + rnd.Next(-10, 11), CultureInfo.InvariantCulture) + "],\"ts\": \"" + 
+                    Timestamp + "\" }";
+                body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Utilities/rabbitmq-tutorials-master/dotnet-visual-studio/2_Send/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+            string message = string.Empty;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                message = "{ \"type\": \"VS\", \"sensorid\" : 2, \"user\": \"b8-27-eb-97-3d-b6\", \"value\":" + i + ", \"ts\": \"3497855005850\" }";
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+                Thread.Sleep(20);
+            }
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
